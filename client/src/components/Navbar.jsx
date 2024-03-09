3ef4fb038a4e7760b506f721a0344ffe4b724b09
Sheeraz ali <61656637+Celestial00@users.noreply.github.com>
2024-03-09 14:58:46 +0500

    initial commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,58 @@
+import { Link, useLocation, useNavigate } from "react-router-dom"
+import { BsSearch } from 'react-icons/bs'
+import { FaBars } from 'react-icons/fa'
+import { RxPencil2 } from "react-icons/rx";
+import { useContext, useState } from "react"
+import Menu from "./Menu"
+import { UserContext } from "../context/UserContext"
+
+
+
+const Navbar = () => {
+
+  const [prompt, setPrompt] = useState("")
+  const [menu, setMenu] = useState(false)
+  const navigate = useNavigate()
+  const path = useLocation().pathname
+
+  // console.log(prompt)
+
+
+  const showMenu = () => {
+    setMenu(!menu)
+  }
+
+
+  const { user } = useContext(UserContext)
+
+  return (
+    <div className="flex items-center justify-between px-6 md:px-[200px] py-8">
+
+      <div className="flex justify-center items-center gap-4">
+
+        <h1 className="text-lg md:text-[1.5em] font-primary font-medium "><Link to="/">Blog</Link></h1>
+        {path === "/" && <div className="flex justify-center items-center bg-gray-100 py-2 px-2 rounded-[10px]  space-x-0">
+         
+          <input onChange={(e) => setPrompt(e.target.value)} className="outline-none bg-gray-100  px-3 " placeholder="Search a post" type="text" />
+          <p onClick={() => navigate(prompt ? "?search=" + prompt : navigate("/"))} className="cursor-pointer"><BsSearch /></p>
+        </div>}
+
+      </div>
+
+      <div className="hidden md:flex items-center justify-center space-x-2 md:space-x-4">
+        {user ? <h3 className="font-primary" ><Link to="/write"><RxPencil2 size={25} /></Link></h3> : <h3><Link to="/login">Login</Link></h3>}
+        {user ? <div onClick={showMenu}>
+          <p className="cursor-pointer relative"><FaBars size={25} /></p>
+          {menu && <Menu />}
+        </div> : <h3 className="font-primary" ><Link to="/register"><RxPencil2 size={30}  /></Link></h3>}
+      </div>
+      <div onClick={showMenu} className="md:hidden text-lg">
+        <p className="cursor-pointer relative"><FaBars size={30} /></p>
+        {menu && <Menu />}
+      </div>
+
+    </div>
+  )
+}
+
+export default Navbar 
---
+++ client/src/pages/CreatePost.jsx
@@ -0,0 +1,110 @@
+
+import Navbar from '../components/Navbar'
+import { CiCirclePlus } from "react-icons/ci";
+
+import { useContext, useState } from 'react'
+import { UserContext } from '../context/UserContext'
+import { URL } from '../url'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
+const CreatePost = () => {
+
+  const [title, setTitle] = useState("")
+  const [desc, setDesc] = useState("")
+  const [file, setFile] = useState(null)
+  const { user } = useContext(UserContext)
+  const [cat, setCat] = useState("")
+  const [cats, setCats] = useState([])
+
+  const navigate = useNavigate()
+
+  const deleteCategory = (i) => {
+    let updatedCats = [...cats]
+    updatedCats.splice(i)
+    setCats(updatedCats)
+  }
+
+  const addCategory = () => {
+    let updatedCats = [...cats]
+    updatedCats.push(cat)
+    setCat("")
+    setCats(updatedCats)
+  }
+
+  const handleCreate = async (e) => {
+    e.preventDefault()
+    const post = {
+      title,
+      desc,
+      username: user.username,
+      userId: user._id,
+      categories: cats
+    }
+
+    if (file) {
+      const data = new FormData()
+      const filename = Date.now() + file.name
+      data.append("img", filename)
+      data.append("file", file)
+      post.photo = filename
+      // console.log(data)
+      //img upload
+      try {
+        const imgUpload = await axios.post(URL + "/api/upload", data)
+        // console.log(imgUpload.data)
+      }
+      catch (err) {
+        console.log(err)
+      }
+    }
+    //post upload
+    // console.log(post)
+    try {
+      const res = await axios.post(URL + "/api/posts/create", post, { withCredentials: true })
+      navigate("/posts/post/" + res.data._id)
+      // console.log(res.data)
+
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
+
+
+
+  return (
+    <div>
+      <Navbar />
+      <div className='px-6 md:px-[200px] mt-8'>
+        <div className=" flex justify-between ">
+
+          <h1 className='font-bold font-primary md:text-[5em] text-xl '>Create a post</h1>
+
+          <div className=" flex justify-center items-center gap-2">
+
+            <label htmlFor="img">
+              <CiCirclePlus size={40} className=' cursor-pointer ' />
+            </label>
+
+            <button onClick={handleCreate} className='bg-black w-full rounded-[10px] md:w-[100px] mx-auto text-white font-semibold px-4 py-2 md:text-xl text-lg'>Create</button>
+            <input id='img' onChange={(e) => setFile(e.target.files[0])} type="file" className='px-4 hidden ' />
+
+
+          </div>
+
+        </div>
+        <form className='w-full flex flex-col space-y-2 md:space-y-1 mt-10'>
+          <input onChange={(e) => setTitle(e.target.value)} type="text" placeholder='Title' className=' text-[50px] font-primary px-4 py-2 outline-none' />
+
+          <textarea onChange={(e) => setDesc(e.target.value)} rows={15} cols={30} className='px-4 text-[20px]  font-primary  outline-none' placeholder='Enter post description' />
+
+        </form>
+
+      </div>
+      {/* <Footer/> */}
+    </div>
+  )
+}
+
+export default CreatePost
